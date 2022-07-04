ee149ec98994318337074e30e8b1e04dbadd76d8
walrusOperator <indierevolver@gmail.com>
2022-07-04 15:35:16 -0500

    Moves files from previous version of project into shelter modules package. Program appears to run the same as revious version. Updated pomxml to use Json-simple and added to requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.ics372_groupv2 {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires json.simple;
 
     opens com.example.ics372_groupv2 to javafx.fxml;
     exports com.example.ics372_groupv2;
