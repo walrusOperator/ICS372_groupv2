3ec445fc32a574d2f5c246c0b113022e3dec9627
KiyokoNee <k.c.gearing@gmail.com>
2022-07-12 14:44:34 -0500

    wrote rough draft of xml to json converter

--- src/main/java/ShelterModules/XMLUtilities.java
+++ src/main/java/ShelterModules/XMLUtilities.java
@@ -1,5 +1,29 @@
 package ShelterModules;
 
+import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
 public class XMLUtilities {
 
+    public JSONObject XMLtoJSON(String filename){
+        File xmlfile = new File(filename);
+        Reader fileReader = new FileReader(xmlfile);
+        BufferedReader bufRead = new BufferedReader(fileReader);
+
+        StringBuilder sb = new StringBuilder();
+        String line = bufRead.readLine();
+        while(line != null){
+            sb.append(line).append("\n");
+            line = bufRead.readLine();
+        }
+
+        String xmlString = sb.toString();
+        System.out.println("XML to String: ");
+        System.out.println(xmlString);
+        return XML.toJSONObject(xmlString);
+    }
 }
